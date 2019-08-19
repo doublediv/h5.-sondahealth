8e06b79753161f25c5d29521f4733fbb73fd4d63
zxc <zhangcanxiao@163.com>
2019-08-19 16:08:41 +0800

    接口换https

--- h5_config.js
+++ h5_config.js
@@ -3,7 +3,7 @@
         // dh: http://192.168.29.61:19092
         // 测试: http://api.sondata.com.cn
         // 服务地址
-        APIBASEURL: "http://api.sondata.com.cn",
+        APIBASEURL: "https://api.sondata.com.cn",
         // 服务超时
         APITIMEOUT: 5000,
     }
